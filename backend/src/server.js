5816ccf442481f24c4add7637477b05eab15ff19
Anderson Mendes <anderson2000ano@gmail.com>
2019-10-09 18:12:38 -0300

    backend changes for frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./router');
+const cors = require('cors');
+
+const path = require('path');
 
 const app = express();
 
@@ -11,7 +14,9 @@ mongoose.connect("mongodb://localhost:27017/aircnc", {
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3000);
